afc1b33d822223604975c38b91b76587b156d0d2
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-11-11 14:13:36 +0800

    HeaderBav - Added logo and Search bar

--- reactClient/components/HeaderNav/HeaderNav.jsx
+++ reactClient/components/HeaderNav/HeaderNav.jsx
@@ -1,5 +1,6 @@
 import {defComponent} from "@/tools/define";
 import classes from "./HeaderNav.module.css";
+import {Icon} from "@iconify-icon/react";
 
 export default defComponent((props) => {
     return (
@@ -8,7 +9,11 @@ export default defComponent((props) => {
                 <h1>Restau<sub>Rants</sub></h1>
             </div>
             <div className={classes.HeaderNavSearch}>
-
+                <Icon icon={"ic:outline-search"} className={classes.SearchIcon}/>
+                <div className={classes.SearchInput}>
+                    <input type={"text"} placeholder={"Search Restaurants, Food, Locations and Tags."}/>
+                    <span className={classes.SearchInputUnderline}></span>
+                </div>
             </div>
         </header>
     )
