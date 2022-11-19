666c89c8e4c9812972e920ab592d0abcd3d58a3f
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-11-19 14:44:48 +0800

    added basepath

--- reactClient/main.jsx
+++ reactClient/main.jsx
@@ -4,20 +4,25 @@ import './index.css'
 import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
 
 import App from "@/views/App";
-const Home = React.lazy(()=>import("@/views/Home/Home"))
+
+const Home = React.lazy(() => import("@/views/Home/Home"))
 
 const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <App/>,
+            children: [
+                {
+                    path: "/",
+                    element: <Home/>
+                }
+            ]
+        },
+    ],
     {
-        path: "/",
-        element: <App/>,
-        children: [
-            {
-                path: "/",
-                element: <Home/>
-            }
-        ]
-    },
-]);
+        basename: import.meta.env.BASE_URL // Set base path so that react router is not confused.
+    }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
