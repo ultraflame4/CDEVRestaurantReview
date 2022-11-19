fb84f902dfce3622a475e5eebea173a295c84376
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-11-19 18:39:48 +0800

    Fixed scrolling issue and added a bottom border for header

--- reactClient/views/Home/Home.jsx
+++ reactClient/views/Home/Home.jsx
@@ -1,14 +1,18 @@
 import {defComponent} from "@/tools/define";
+import "./Home.css"
 
 export default defComponent((props) => {
     return (
-        <div>
+        <main>
+            <section id={"recommendations-banner"}>
+
+            </section>
             <h1>Test</h1>
             <h2>Test</h2>
             <h3>Test</h3>
             <h4>Test</h4>
             <h5>Test</h5>
             <h6>Test</h6>
-        </div>
+        </main>
     )
 })
