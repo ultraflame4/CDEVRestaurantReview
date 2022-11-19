138f8d75580a39afe88ff27c9cf482f46eee6b9c
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-11-19 14:45:18 +0800

    fixed a couple of issues relating to react routing and express routing

--- server/app.js
+++ server/app.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config() // load env variables
 
 const app = express()
 const port = process.env.VITE_EXPRESS_PORT
 
-app.use(express.static('dist'))
+
 
 // Set up cross site origin so that the client development server can access api
 const allowedOrigins = [
     `http://localhost:${process.env.VITE_DEV_PORT}`,
     `http://127.0.0.1:${process.env.VITE_DEV_PORT}`,
 ]
+
+// Serve the static files
+app.use("/app",express.static(path.join(__dirname,'../dist/')))
 
 app.use(cors({
     origin: (origin, callback) => {
@@ -22,10 +26,15 @@ app.use(cors({
         }
         console.log(`CORS error: origin ${origin} not allowed`)
         return callback(new Error("The CORS policy for this site does not allow access from the specified Origin"), true)
-    }
+    },
+
 }))
 
 
+app.get('/app/*', (req, res) => {
+    // When refreshing in the app path, redirect back to react client
+    res.sendFile(path.join(__dirname,'../dist/index.html'))
+})
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
