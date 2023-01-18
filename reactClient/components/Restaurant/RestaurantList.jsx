fd5eba20ce94295b136345ca2ad3d9192decdf95
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-01-18 18:30:22 +0800

    Moved filter panel code into another file(s). Restaurant content items sizes are now dynamic

--- reactClient/components/Restaurant/RestaurantList.jsx
+++ reactClient/components/Restaurant/RestaurantList.jsx
@@ -1,92 +1,13 @@
 import PropTypes, {func, number} from "prop-types";
 import classes from "./Restaurant.module.css"
-import {InputRangeSlider} from "@/components/InputRangeSlider/InputRangeSlider";
+
 import {Icon} from "@iconify-icon/react";
 import React, {useEffect, useRef, useState} from "react";
 import {GetRestaurants} from "@/api";
 import {StarRatings} from "@/components/Ratings/StarRatings";
 import {CostRatings} from "@/components/Ratings/CostRatings";
 import {LineBreaker} from "@/components/LineBreaker";
-
-/**
- * @param {{
- *     title: string,
- *     icon: string,
- *     minIcon?: string,
- *     className?: string,
- *     min?: number,
- *     max?: number,
- *     step?: number,
- *     defaultValue?: number,
- *     callback?: function (value:number, min:number, max :number) : void,
- *     noIcon?: boolean,
- *     minText?: string,
- *     maxText?: string,
- *     showValue?: boolean,
- *     showValueIcon?: string,
- *     showValueMaxText?: string
- * }} props
- * @return {*}
- * @constructor
- */
-const FilterSliderItem = (props) => {
-    const [valueText, setValueText] = useState(0)
-
-    function callback(value, min, max) {
-        setValueText(value !== max ? value : (props.showValueMaxText ?? value))
-        props.callback?.(value, min, max)
-    }
-
-    return (<li className={props.className}>
-        <h5>{props.title}</h5>
-        {!props.showValue ? "" : <span className={classes.filterRangeSliderValue}>
-                        {valueText}
-            {props.showValueIcon ? <Icon icon={props.showValueIcon} className={classes.icon}/> : ""}
-                    </span>}
-        <InputRangeSlider min={props.min} max={props.max} step={props.step} defaultValue={props.defaultValue}
-                          onInput={callback}/>
-        {props.noIcon ? <>
-                        <span>
-                            {props.minText}
-
-                        </span>
-            <div>
-                            <span>
-                                {props.maxText}
-                            </span>
-            </div>
-        </> : <>
-            <Icon icon={props.minIcon ?? props.icon} className={classes.icon}/>
-            <div>
-                <Icon icon={props.icon} className={classes.icon}/>
-                <Icon icon={props.icon} className={classes.icon}/>
-                <Icon icon={props.icon} className={classes.icon}/>
-                <Icon icon={props.icon} className={classes.icon}/>
-                <Icon icon={props.icon} className={classes.icon}/>
-            </div>
-        </>}
-    </li>)
-}
-
-
-FilterSliderItem.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    minIcon: PropTypes.string,
-    className: PropTypes.string,
-    min: PropTypes.number,
-    max: PropTypes.number,
-    step: PropTypes.number,
-    defaultValue: PropTypes.number,
-    callback: PropTypes.object,
-    noIcon: PropTypes.bool,
-    minText: PropTypes.string,
-    maxText: PropTypes.string,
-    showValue: PropTypes.bool,
-    showValueIcon: PropTypes.string,
-    showValueMaxText: PropTypes.string
-}
-
+import {FilterSidepanel} from "@/components/Restaurant/SidePanelFilter";
 
 const RestaurantListItem = (props) => {
 
@@ -148,15 +69,16 @@ const RestaurantListContents = (props) => {
                                     name={value.name}
                                     desc={value.description}
                                     imageSrc={value.photo_url}
-                                    distance={Math.round(value.distance/1000)}
+                                    distance={Math.round(value.distance / 1000)}
                 />)
         }
     </ul>)
 }
 
-RestaurantListContents.propTypes={
-    restaurants:PropTypes.array.isRequired
+RestaurantListContents.propTypes = {
+    restaurants: PropTypes.array.isRequired
 }
+
 
 export const RestaurantList = (props) => {
 
@@ -175,46 +97,7 @@ export const RestaurantList = (props) => {
         <h1 className={classes.title}>
             {props.title}
         </h1>
-        <aside className={`card ${classes.filterpanel}`}>
-            <h4>Filters</h4>
-            <ul>
-                <FilterSliderItem
-                    className={classes.costSection}
-                    title={"Cost"}
-                    icon={"fa:dollar"}
-                    min={1}
-                    max={5}
-                />
-                <FilterSliderItem
-                    className={classes.starsSection}
-                    title={"Min. Ratings"}
-                    minIcon={"material-symbols:star-outline"}
-                    icon={"material-symbols:star"}
-                    showValueIcon={"material-symbols:star"}
-                    min={0}
-                    max={5}
-                    showValue={true}
-
-                />
-
-                <FilterSliderItem
-                    className={classes.reviewsSection}
-                    title={"Min. Reviews"}
-                    minIcon={"material-symbols:star-outline"}
-                    icon={"material-symbols:star"}
-                    min={0}
-                    max={41}
-                    noIcon={true}
-                    minText={"0"}
-                    maxText={"40+"}
-                    showValueMaxText={"40+"}
-                    showValue={true}
-                    showValueIcon={"material-symbols:chat"}
-                />
-
-            </ul>
-            <h4>Sort by</h4>
-        </aside>
+        <FilterSidepanel/>
         <RestaurantListContents restaurants={restaurants}/>
     </div>;
 }
---
+++ reactClient/components/Restaurant/SidePanelFilter.jsx
@@ -0,0 +1,129 @@
+import React, {useState} from "react";
+
+import {Icon} from "@iconify-icon/react";
+import {InputRangeSlider} from "@/components/InputRangeSlider/InputRangeSlider";
+import PropTypes from "prop-types";
+import classes from "@/components/Restaurant/SidePanelFilter.module.css";
+
+/**
+ * @param {{
+ *     title: string,
+ *     icon: string,
+ *     minIcon?: string,
+ *     className?: string,
+ *     min?: number,
+ *     max?: number,
+ *     step?: number,
+ *     defaultValue?: number,
+ *     callback?: function (value:number, min:number, max :number) : void,
+ *     noIcon?: boolean,
+ *     minText?: string,
+ *     maxText?: string,
+ *     showValue?: boolean,
+ *     showValueIcon?: string,
+ *     showValueMaxText?: string
+ * }} props
+ * @return {*}
+ * @constructor
+ */
+const FilterSliderItem = (props) => {
+    const [valueText, setValueText] = useState(0)
+
+    function callback(value, min, max) {
+        setValueText(value !== max ? value : (props.showValueMaxText ?? value))
+        props.callback?.(value, min, max)
+    }
+
+    return (<li className={props.className}>
+        <h5>{props.title}</h5>
+
+        {!props.showValue ?
+            "" :
+            <span className={classes.filterRangeSliderValue}>
+                {valueText}
+                {props.showValueIcon ? <Icon icon={props.showValueIcon} className={classes.icon}/> : ""}
+            </span>
+        }
+
+        <InputRangeSlider min={props.min} max={props.max} step={props.step} defaultValue={props.defaultValue}
+                          onInput={callback}/>
+        {props.noIcon ? <>
+            <span>{props.minText}</span>
+            <div><span>{props.maxText}</span></div>
+        </> : <>
+            <Icon icon={props.minIcon ?? props.icon} className={classes.icon}/>
+            <div>
+                <Icon icon={props.icon} className={classes.icon}/>
+                <Icon icon={props.icon} className={classes.icon}/>
+                <Icon icon={props.icon} className={classes.icon}/>
+                <Icon icon={props.icon} className={classes.icon}/>
+                <Icon icon={props.icon} className={classes.icon}/>
+            </div>
+        </>}
+    </li>)
+}
+
+
+FilterSliderItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    minIcon: PropTypes.string,
+    className: PropTypes.string,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number,
+    defaultValue: PropTypes.number,
+    callback: PropTypes.object,
+    noIcon: PropTypes.bool,
+    minText: PropTypes.string,
+    maxText: PropTypes.string,
+    showValue: PropTypes.bool,
+    showValueIcon: PropTypes.string,
+    showValueMaxText: PropTypes.string
+}
+
+export const FilterSidepanel = (props) => {
+
+    return (
+        <aside className={`card ${classes.filterpanel}`}>
+            <h4>Filters</h4>
+            <ul>
+                <FilterSliderItem
+                    className={classes.costSection}
+                    title={"Cost"}
+                    icon={"fa:dollar"}
+                    min={1}
+                    max={5}
+                />
+                <FilterSliderItem
+                    className={classes.starsSection}
+                    title={"Min. Ratings"}
+                    minIcon={"material-symbols:star-outline"}
+                    icon={"material-symbols:star"}
+                    showValueIcon={"material-symbols:star"}
+                    min={0}
+                    max={5}
+                    showValue={true}
+
+                />
+
+                <FilterSliderItem
+                    className={classes.reviewsSection}
+                    title={"Min. Reviews"}
+                    minIcon={"material-symbols:star-outline"}
+                    icon={"material-symbols:star"}
+                    min={0}
+                    max={41}
+                    noIcon={true}
+                    minText={"0"}
+                    maxText={"40+"}
+                    showValueMaxText={"40+"}
+                    showValue={true}
+                    showValueIcon={"material-symbols:chat"}
+                />
+
+            </ul>
+            <h4>Sort by</h4>
+        </aside>
+    )
+}
