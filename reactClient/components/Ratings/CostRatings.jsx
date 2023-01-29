835ba118b0d36df88893c25f1f6a16ee4af4f119
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-01-29 13:47:03 +0800

    added comments

--- reactClient/components/Ratings/CostRatings.jsx
+++ reactClient/components/Ratings/CostRatings.jsx
@@ -10,10 +10,10 @@ import React from "react";
  */
 
 /**
- *
+ * This component displays a rating of 1-5 dollar signs
+ * So if there is a rating of 3, it will display 3 dollar signs
  * @param props {CostRatingsProp}
- *
- * @constructor
+ * @returns {React.ReactNode}
  */
 export const CostRatings = (props) => {
     /**
@@ -21,10 +21,12 @@ export const CostRatings = (props) => {
      */
     let stars = []
 
+    // If the rating is less than 1, then we don't know the cost
     if (props.rating<1){
         return <div className={classes.costRating}>?</div>
     }
 
+    // basically, we just add a dollar sign every loop
     for (let i = 0; i < props.rating; i++) {
         stars.push(<Icon icon={"fa:dollar"} className={classes.costRating} key={i}/>)
     }
--- reactClient/components/Ratings/StarRatings.jsx
+++ reactClient/components/Ratings/StarRatings.jsx
@@ -10,24 +10,30 @@ import React from "react";
  */
 
 /**
- *
+ * Similar to CostRatings, but uses stars instead of dollar signs and the number is from 1-10 instead of 1-4
  * @param props {StarRatingsProp}
- *
- * @constructor
+ * @returns {React.ReactNode}
  */
 export const StarRatings = (props) => {
     /**
      * @type {JSX.Element[]}
      */
     let stars = []
+
+    // number of stars left to display
     let stars_count = props.rating / 2
+
+    // Loop 5 times to display 5 stars
     for (let i = 0; i < 5; i++) {
+        // If the no of stars left is 0.5, then we need to display a half star
         if (stars_count === 0.5) {
             stars.push(<Icon key={i} icon={"material-symbols:star-half"} className={classes.starRating}/>)
         }
+        // If the no of stars left is greater than 0, then we need to display a full star
         else if (stars_count > 0) {
             stars.push(<Icon key={i} icon={"material-symbols:star"} className={classes.starRating}/>)
         }
+        // If no more stars left, then we need to display an empty star
         else{
             stars.push(<Icon key={i} icon={"material-symbols:star-outline"} className={classes.starRating}/>)
         }
