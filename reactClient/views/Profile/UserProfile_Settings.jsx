27639c88826482568db62cfbec20a67a344e2329
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-02-02 20:54:10 +0800

    Added stuff to profile page

--- reactClient/views/Profile/UserProfile_Settings.jsx
+++ reactClient/views/Profile/UserProfile_Settings.jsx
@@ -1,5 +1,22 @@
-export default function UserProfile_Settings() {
-    return (<main>
+import classes from "./Settings.module.css";
+import {useContext} from "react";
+import {UserAccountContext} from "@/tools/contexts";
+import {Icon} from "@iconify-icon/react";
 
+export default function UserProfile_Settings() {
+    const currentUser = useContext(UserAccountContext);
+    return (<main className={classes.main}>
+        <div>
+            <h2>Username :</h2><p>{currentUser?.username}</p>
+        </div>
+        <button>Edit Username <Icon icon={"ic:baseline-edit"}/></button>
+        <div>
+            <h2>Email :</h2><p>{currentUser?.email}</p>
+        </div>
+        <button>Change Email <Icon icon={"ic:baseline-edit"}/></button>
+        <div>
+            <h2>Password</h2>
+        </div>
+        <button>Update Password <Icon icon={"ic:baseline-edit"}/></button>
     </main>)
 }
