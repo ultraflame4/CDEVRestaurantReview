49e2d684a7a65543f5c6f69dd3b65a3a81a0730d
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-02-04 15:18:34 +0800

    finished nearby restaurants banner. Left top rated

--- reactClient/components/RecommendedBanner/RecommendedItems.jsx
+++ reactClient/components/RecommendedBanner/RecommendedItems.jsx
@@ -4,26 +4,62 @@ import {GetRestaurantRecentReviews} from "@/core/api";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import classes from "./RecommendedItems.module.css";
+import {StarRatings} from "@/components/Ratings/StarRatings";
+import {LineBreaker} from "@/components/LineBreaker";
+import {Icon} from "@iconify-icon/react";
+import {CostRatings} from "@/components/Ratings/CostRatings";
 
 export function RecommendedNearbyItem(props) {
 
-    const [recentReviews, setRecentReviews] = useState([])
+    const [recentReview, setRecentReview] = useState("")
 
     useEffect(() => {
-        GetRestaurantRecentReviews(props.id).then(reviews => {
-            setRecentReviews(reviews)
+        GetRestaurantRecentReviews(props.id, 1).then(reviews => {
+            console.log(reviews)
+            setRecentReview(reviews[0]?.content ?? "No reviews yet")
         })
     }, [props.id])
 
     return (
-        <li  className={`${classes.nearbyItem} card`}>
+        <li className={`${classes.nearbyItem} card`}>
             <Link to={`/restaurant/${props.id}`}>
+                <img src={props.imageSrc}/>
+                <div className={classes.nearbyItemInfoCtn}>
+                    <h3>{props.name}</h3>
+                    <div className={classes.nearbyItemRating}><span>{props.reviews_count}</span><StarRatings
+                        rating={props.rating}/></div>
 
+                    {/*Tags here*/}
+                    <ul className={classes.restaurant_tagslist}>
+                        { // For each tag given in the tag list in props, create a li for it
+                            props.tags?.map((tagname, tagindex) => {
+                                return (<li key={tagindex}>
+                                    {tagname}
+                                </li>)
+                            })
+                        }
+                    </ul>
+                    <p>
+                        <LineBreaker text={props.desc} sep={"<br>"}/>
+                    </p>
+                    <div className={classes.review}>
+                        <Icon icon={"ic:chat"} className={classes.reviewIcon}/>
+                        <p>
+                            <LineBreaker text={recentReview} sep={"<br>"}/>
+                        </p>
+                        <span/>
+                    </div>
+                    <span className={classes.nearbyItemCost}><CostRatings rating={props.cost}/></span>
+                    <span className={classes.nearbyItemDistance}>{props.distance}km</span>
+
+
+                </div>
             </Link>
         </li>
     )
 
 }
+
 RecommendedNearbyItem.propType = {
     id: PropTypes.number.isRequired,
     tags: PropTypes.arrayOf(PropTypes.string).isRequired,
